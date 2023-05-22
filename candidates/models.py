a226d748798f944ead0de356d0da3121ce8cd0ea
HamzaKhan001 <spaceus9@gmail.com>
2023-05-23 00:11:19 +0500

    import django_countries

--- candidates/models.py
+++ candidates/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from autoslug import AutoSlugField
+from django_countries.fields import CountryField
 
 
 # Create your models here.
