459eb7d497f095ae50f9da956bdd38f4a10ea280
HamzaKhan001 <spaceus9@gmail.com>
2023-05-23 00:26:11 +0500

    profile model completed

--- candidates/models.py
+++ candidates/models.py
@@ -18,4 +18,11 @@ CHOICES=(
 
 class Profile(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True, related_name='profile')
+    full_name=models.CharField(max_length=200, null=True, blank=True)
+    country=CountryField(null=True,blank=True)
+    location=models.CharField(max_length=255, null=True, blank=True)
+    resume=models.FileField(upload_to="resumes",null=True,blank=True)
+    grad_year = models.IntegerField(blank=True)
+    looking_for=models.CharField(max_length=30, choices=CHOICES, default='Full Time',null=True)
+    slug=AutoSlugField(populate_from='user',unique=True)
     
