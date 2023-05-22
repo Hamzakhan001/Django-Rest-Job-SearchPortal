90f802dea1a67d3a28bafecbe1372faf3c047e54
HamzaKhan001 <spaceus9@gmail.com>
2023-05-23 00:20:37 +0500

    choices list

--- candidates/models.py
+++ candidates/models.py
@@ -3,6 +3,14 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 from autoslug import AutoSlugField
 from django_countries.fields import CountryField
+from recruiters.models import Job
+from django.utils import timezone
 
 
-# Create your models here.
+
+CHOICES=(
+	('Full Time','Full Time'),
+	('Part Time','Part Time'),
+	('Internship','Internship'),
+	('Remote','Remote'),
+)
