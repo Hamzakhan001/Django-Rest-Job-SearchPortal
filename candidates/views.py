be5a844ebf5f08e8df1b0dfeeda5020461ddd02d
HamzaKhan001 <spaceus9@gmail.com>
2023-05-24 11:10:55 +0500

    candidate app views

--- candidates/views.py
+++ candidates/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Job
 
-# Create your views here.
+
+
+
+
+def job_search_list(request):
+    query=request.GET.get('p')
+    loc = request.GET.get('q')
+    object_list=[]
+    
+    if(query == None):
+        object_list= Job.objects.all
+    else :
+        title_list=Job.objects.filter(title__icontains=query).order_by('date_posted')
+        skill_list=Job.objects.filter(skill_req__icontains=query).order_by('date_posted')
+        job_type_list=Job.objects.filter(job_type__icontains=query).order_by('date_posted')
+        company_list=Job.objects.filter(job_type__icontains==query).order_by('-date_posted')
+        
+        
